7dc919d72215e426047a7e16fdb2d25745be037b
minhye9731 <jenney62639731@gmail.com>
2023-06-10 14:59:48 +0900

    리스트뷰 생성

--- RUT/RUT/MainView.swift
+++ RUT/RUT/MainView.swift
@@ -8,9 +8,31 @@
 import SwiftUI
 
 struct MainView: View {
+    
+    var dummyData: [ListItemView] = [
+        ListItemView(mbti: "INFP", nickName: "나는야INFP", description: "하하하하하하하하"),
+        ListItemView(mbti: "INFP", nickName: "나는야INFP", description: "하하하하하하하하"),
+        ListItemView(mbti: "INFP", nickName: "나는야INFP", description: "하하하하하하하하"),
+        ListItemView(mbti: "INFP", nickName: "나는야INFP", description: "하하하하하하하하"),
+        ListItemView(mbti: "INFP", nickName: "나는야INFP", description: "하하하하하하하하")
+    ]
+    
     var body: some View {
-        Text("MAIN화면이다!! 여기에 scrollview, lazy Vstack??")
-            .font(.largeTitle)
-            .foregroundColor(.yellow)
+        
+        // 리스트뷰
+        ZStack(alignment: .top) {
+            ScrollView {
+                LazyVStack(spacing: 20) {
+                    ForEach(dummyData) { item in
+                        ListItemView(mbti: item.mbti, nickName: item.nickName, description: item.description)
+                    }
+                }
+            }
+        }
+        
+        // 공유하기 버튼
+        
+        
     }
+    
 }
