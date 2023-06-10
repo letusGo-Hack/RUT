138dfceb7b88cf58dd9b43cd945b8899e0b0d803
minhye9731 <jenney62639731@gmail.com>
2023-06-10 15:48:39 +0900

    메인화면 공유하기 버튼 추가

--- RUT/RUT/MainView.swift
+++ RUT/RUT/MainView.swift
@@ -9,30 +9,31 @@ import SwiftUI
 
 struct MainView: View {
     
-    var dummyData: [ListItemView] = [
-        ListItemView(mbti: "INFP", nickName: "나는야INFP", description: "하하하하하하하하"),
-        ListItemView(mbti: "INFP", nickName: "나는야INFP", description: "하하하하하하하하"),
-        ListItemView(mbti: "INFP", nickName: "나는야INFP", description: "하하하하하하하하"),
-        ListItemView(mbti: "INFP", nickName: "나는야INFP", description: "하하하하하하하하"),
-        ListItemView(mbti: "INFP", nickName: "나는야INFP", description: "하하하하하하하하")
-    ]
+    let values: [ListItemView]
     
     var body: some View {
-        
-        // 리스트뷰
-        ZStack(alignment: .top) {
-            ScrollView {
-                LazyVStack(spacing: 20) {
-                    ForEach(dummyData) { item in
-                        ListItemView(mbti: item.mbti, nickName: item.nickName, description: item.description)
+        NavigationView {
+
+            VStack {
+                // 리스트뷰
+                ScrollView {
+                    LazyVStack() {
+                        ForEach(values) { item in
+                            ListItemView(mbti: item.mbti, nickName: item.nickName, description: item.description)
+                        }
                     }
                 }
+                
+                // 공유하기 버튼
+                Button("공유하기") {
+                    print("공유하기 버튼 클릭!!")
+                }
+                .padding(16)
             }
+            .navigationBarTitle("지갑")
+            
+            
         }
-        
-        // 공유하기 버튼
-        
-        
     }
     
 }
