09db56c30046e1a7d560f2dc90e46d66e386746e
minhye9731 <jenney62639731@gmail.com>
2023-06-10 14:05:18 +0900

    메인뷰 생성

---
+++ RUT/RUT/MainView.swift
@@ -0,0 +1,16 @@
+//
+//  MainView.swift
+//  RUT
+//
+//  Created by 강민혜 on 6/10/23.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("MAIN화면이다!! 여기에 scrollview, lazy Vstack??")
+            .font(.largeTitle)
+            .foregroundColor(.yellow)
+    }
+}
